d26944f403481fb01d6c24d28278ce8a291ef647
ernestob445 <ernestoblanco445@gmail.com>
2020-09-03 13:40:07 -0600

    busqueda por fecha

--- src/NASAClient.js
+++ src/NASAClient.js
@@ -1,13 +1,16 @@
 class NASAClient {
-
-    getAstrononyPictureOfTheDay() {
-        const dateToday = new Date().toISOString().slice(0, 10);
-        const url = `https://api.nasa.gov/planetary/apod?date=${dateToday}&api_key=o4zE7xs1zFU20T7ITAyRVOYexcw5cTbnIdb5C3FB`
-        return fetch(url)
-            .then(res => res.json())
-            .then((result) => {
-                return result
-            })
+    getAstrononyPictureOfTheDay(dateNew) {
+        const url = `https://api.nasa.gov/planetary/apod?date=${dateNew}&api_key=o4zE7xs1zFU20T7ITAyRVOYexcw5cTbnIdb5C3FB`
+        try {
+            return fetch(url)
+                .then(res => res.json())
+                .then((result) => {
+                    return result
+                })
+        }
+        catch (e) {
+            console.log(e)
+        }
     }
 }
 export default NASAClient
--- src/pages/APODPage/APODPage.jsx
+++ src/pages/APODPage/APODPage.jsx
@@ -1,27 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import NASAClient from '../../NASAClient'
-import { ResponsiveEmbed, CardImg } from 'react-bootstrap';
+import { ResponsiveEmbed, CardImg, Row, Col, Form } from 'react-bootstrap';
 
 
 const APODPage = () => {
-    const [data, setData] = useState([])
+    const [data, setData] = useState({})
+    const [date, setDate] = useState(new Date().toISOString().slice(0, 10))
 
     useEffect(() => {
         const NASAClientApi = new NASAClient()
-        NASAClientApi.getAstrononyPictureOfTheDay().then(response => setData(response))
-    }, []);
+        NASAClientApi.getAstrononyPictureOfTheDay(date).then(response => setData(response))
+    }, [date]);
+
+
+    /*  handleDateNew=()=>{
+         const date = document.getElementById("dateNew").value;
+         setDateNew(document.getElementById("dateNew").value);
+         console.log(dateNew);
+     } */
 
     //<img className="responsive" style={{ width: "100%" }} src={data.hdurl} alt="" />
     return (
         <>
-            <div>
-                <h4>{data.date}</h4>
-                <h1>{data.title}</h1>
-                <div className="w-75">
-                    {(data.media_type === "image") ? <CardImg style={{ width: "100%" }} src={data.hdurl} alt="NASA" /> : <ResponsiveEmbed aspectRatio="16by9"><embed className="w-100 p-3 h-100" src={data.url} type="" /></ResponsiveEmbed>}
-                <p className="mt-5 text-justify">{data.explanation}</p>
-                </div>
-            </div>
+            <Row>
+                <Col md={{ span: 8, offset: 2 }} >
+                    <h4>{data.date}</h4>
+                    <h1>{data.title}</h1>
+                    <div>
+                        {(data.media_type === "image") ? <CardImg style={{ width: "100%" }} src={data.hdurl} alt="NASA" /> : <ResponsiveEmbed aspectRatio="16by9"><embed className="w-100 p-3 h-100" src={data.url} type="" /></ResponsiveEmbed>}
+                        <div className="mt-3 text-right offset-md-6 col-auto">
+                            <Form>
+                                <Form.Row className="align-items-center">
+                                    <Col >
+                                        <Form.Label htmlFor="dateNew" srOnly>
+                                            Date
+                                </Form.Label>
+                                        <Form.Control
+                                            type="date"
+                                            className="mb-2"
+                                            id="dateNew"
+                                            min="1995-06-16"
+                                            value={new Date().toISOString().slice(0, 10)}
+                                            max={new Date().toISOString().slice(0, 10)}
+                                            placeholder="Date"
+                                            onChange={() => {
+                                                setDate(document.getElementById("dateNew").value)
+                                            }}
+                                        />
+                                    </Col>
+                                </Form.Row>
+                            </Form>
+                        </div>
+                        <p className="mt-4 text-justify">{data.explanation}</p>
+                    </div>
+                </Col>
+            </Row>
         </>
     )
 }
